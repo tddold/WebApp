1cda1ba27d471a25b6a3a4978068c2bbf2c5ab20
tddold <tdd@dekom.bg>
2017-04-16 16:55:55 +0300

    Add IntegrationTets

---
+++ source/App/IntegrationTests/App.IntegrationTests.IntegrationTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("App.IntegrationTests.IntegrationTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("App.IntegrationTests.IntegrationTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6c630f8b-7c2f-4f02-8ec7-5e8083ab7acd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ source/App/IntegrationTests/App.IntegrationTests.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using App.Data;
+
+namespace App.IntegrationTests.IntegrationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            AppDbContext context = new AppDbContext();
+
+            // At
+            int usersCount = context.Users.Count();
+
+            // Assert
+            Assert.AreEqual(6, usersCount);
+        }
+    }
+}
