b72205b5b6169d284a29f231dae421bbca75afbd
tddold <tdd@dekom.bg>
2017-05-22 03:24:31 +0300

    Add home template

--- source/App/Web/App.Web/App_Start/BundleConfig.cs
+++ source/App/Web/App.Web/App_Start/BundleConfig.cs
@@ -11,8 +11,22 @@ namespace App.Web
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/them").Include(
+                       "~/assets/js/jquery - 1.11.1.js",
+                       "~/assets/js/bootstrap.js",
+                       "~/assets/js/jquery.easing.min.js",
+                       "~/assets/js/jquery.isotope.js",
+                       "~/assets/js/appear.min.js",
+                       "~/assets/js/animations.min.js",
+                       "~/assets/js/custom.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/themvega").Include(
+                     "~/assets/js/vegas/jquery.vegas.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/themfan").Include(
+                    "~/assets/js/source/jquery.fancybox.js"));
+
+
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -23,9 +37,28 @@ namespace App.Web
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/bootstrapthem").Include(
+                     "~/assets/css/bootstrap.js"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/ionicons").Include(
+                     "~/assets/css/ionicons.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/font-awesome").Include(
+                     "~/assets/css/font-awesome.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/animations").Include(
+                     "~/assets/css/animations.min.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/blue").Include(
+                     "~/assets/css/style-blue.css"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/themjq").Include(
+                  "~/assets/js/source/jquery.fancybox.css"));
         }
     }
 }
